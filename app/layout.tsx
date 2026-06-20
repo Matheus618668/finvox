cdfa86d2ba5c08b9f084de6c12b73c9f18e5268e
Matheus618668 <matheusneves635@gmail.com>
2026-06-19 21:27:55 -0300

    fix: absolute restore of layout.tsx module

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import React from 'react'
+
+export const metadata = {
+  title: 'FinVox',
+  description: 'Gestão financeira por voz',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
